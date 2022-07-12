e2fac1eda6e394f566e92221ef71b7a445156b8a
Maddy <zyfroxx@gmail.com>
2022-07-12 11:19:32 +0530

    created a database

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
 
 
 @app.route('/')
