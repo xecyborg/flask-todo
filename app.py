499da4087bc3055fd53190c958a240de8b38de51
Maddy <zyfroxx@gmail.com>
2022-07-14 11:57:04 +0530

    Update functionality is now working

--- app.py
+++ app.py
@@ -31,6 +31,22 @@ def hello_world():
     return render_template('index.html', allTodo=allTodo)
 
 
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/')
+
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template('update.html', todo=todo)
+
+
 @app.route('/delete/<int:sno>')
 def delete(sno):
     todo = Todo.query.filter_by(sno=sno).first()
